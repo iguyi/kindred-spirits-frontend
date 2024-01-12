c5a42366932597cfcb87e2e6be3e83c85ce0c6e1
guyi <1802366955@qq.com>
2024-01-12 20:50:59 +0800

    Axios 封装

---
+++ src/plugins/myAxios.ts
@@ -0,0 +1,38 @@
+import axios, {AxiosInstance} from "axios";
+
+const myAxios: AxiosInstance = axios.create({
+    baseURL: 'http://localhost:8080/kindredspirits'
+});
+
+// 发送请求时携带 cookie
+myAxios.defaults.withCredentials = true;
+
+// 添加请求拦截器
+myAxios.interceptors.request.use(function (config) {
+    // 在请求发送前执行的任务
+    console.log('请求已发送');
+    return config;
+}, function (error) {
+    // 处理请求错误
+    return Promise.reject(error);
+});
+
+// 添加响应拦截器
+myAxios.interceptors.response.use(function (response) {
+    // 处理响应数据
+    console.log('接收到响应数据\n', response);
+    // 未登录
+    if (response?.data?.code === 40100) {
+        const redirectUrl = window.location.href;
+        window.location.href = `/user/login?redirect=${redirectUrl}`;
+    }
+
+    // return response.data;  // SearchResultPage 会出问题
+    return response;
+}, function (error) {
+    // 处理响应错误
+    console.log('响应出错');
+    return Promise.reject(error);
+});
+
+export default myAxios;
