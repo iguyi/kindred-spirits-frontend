6a8262650bab5fbe1d928c89f9261bf43e35ed6b
guyi <1802366955@qq.com>
2024-01-12 20:39:41 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import BasicLayout from "./layouts/BasicLayout.vue";
+</script>
+
+<template>
+  <!-- 用户已登录 -->
+  <template v-if="1===1">
+    <BasicLayout/>
+  </template>
+</template>
+
+<style scoped>
+</style>
