976de9da9d98dedb8a102332440df9e781e457c9
guyi <1802366955@qq.com>
2024-01-12 20:58:12 +0800

    队伍列表组件 - 用于通讯录展示

---
+++ src/components/TeamCardListSimple.vue
@@ -0,0 +1,61 @@
+<template>
+  <van-cell
+      v-for="team in teamList"
+      center
+  >
+    <template #right-icon>
+    </template>
+    <template #extra>
+      <van-button
+          type="primary"
+          size="mini"
+          @click="look"
+      >
+        查看
+      </van-button>
+    </template>
+    <template #default>
+      <div class="custom-cell">
+        <div>
+          <van-image
+              round
+              width="30px"
+              height="30px"
+              src="http://localhost:3000/public/avatar/team/default_avatar.jpg"
+          />
+        </div>
+        <div style="margin-left: 5px">
+          <span class="content">{{ team.name }}</span><br>
+        </div>
+      </div>
+    </template>
+  </van-cell>
+</template>
+
+<script setup lang="ts">
+import {TeamType} from "../models/team";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+interface TeamCardListSimpleProps {
+  teamList: TeamType[];
+}
+
+const props = withDefaults(defineProps<TeamCardListSimpleProps>(), {
+  // @ts-ignore
+  teamList: [] as TeamType[],
+});
+</script>
+
+<style scoped>
+.custom-cell {
+  display: flex;
+  align-items: center; /* 垂直居中对齐 */
+}
+
+.content {
+  margin-right: 4px;
+  font-size: 20px;
+}
+</style>
