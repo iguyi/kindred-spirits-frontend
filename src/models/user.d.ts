2ec27291e8fa108c3e1903e899582d4534cde4ce
guyi <1802366955@qq.com>
2024-01-12 20:54:42 +0800

    用户类型

---
+++ src/models/user.d.ts
@@ -0,0 +1,21 @@
+/**
+ * 用户类型
+ */
+export type UserType = {
+    id: number;
+    username: string;
+    userAccount: string;
+    avatarUrl: string;
+    gender: number;
+    userRole: number;
+    tags: string[];
+    profile: string;
+    phone: string;
+    email: string;
+    createTime: Date;
+}
+
+/*
+{
+    "userStatus": 0.0,
+}*/
