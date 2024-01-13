7fe10d03fb387bf1149e43053f0cfa0997c286c0
guyi <1802366955@qq.com>
2024-01-13 20:13:27 +0800

    补充注册功能

--- src/pages/user/UserLoginPage.vue
+++ src/pages/user/UserLoginPage.vue
@@ -1,28 +1,71 @@
 <template>
-  <van-form @submit="onSubmit">
-    <van-cell-group inset>
-      <van-field
-          v-model="userAccount"
-          name="userAccount"
-          label="账号"
-          placeholder="请填写账号"
-          :rules="[{ required: true, message: '请填写账号' }]"
-      />
-      <van-field
-          v-model="userPassword"
-          type="password"
-          name="userPassword"
-          label="密码"
-          placeholder="请填写密码"
-          :rules="[{ required: true, message: '请填写密码' }]"
-      />
-    </van-cell-group>
-    <div style="margin: 16px;">
-      <van-button round block type="primary" native-type="submit">
-        登录
-      </van-button>
-    </div>
-  </van-form>
+
+  <van-tabs v-model:active="active" swipeable color="#00994C" type="card">
+    <van-tab title="登录">
+      <br/>
+      <div>
+        <van-form @submit="onLoginSubmit">
+          <van-cell-group inset>
+            <van-field
+                v-model="loginUserAccount"
+                name="userAccount"
+                label="账号"
+                placeholder="请填写账号"
+                :rules="[{ required: true, message: '请填写账号' }]"
+            />
+            <van-field
+                v-model="loginUserPassword"
+                type="password"
+                name="userPassword"
+                label="密码"
+                placeholder="请填写密码"
+                :rules="[{ required: true, message: '请填写密码' }]"
+            />
+          </van-cell-group>
+
+          <div style="margin: 16px;">
+            <van-button round block type="primary" native-type="submit">
+              登录
+            </van-button>
+          </div>
+        </van-form>
+      </div>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    </van-tab>
+
+    <van-tab title="注册">
+      <br/>
+      <div>
+        <van-form @submit="onRegisterSubmit">
+          <van-cell-group inset>
+            <van-field
+                v-model="registerUserPassword"
+                type="password"
+                name="userPassword"
+                label="密码"
+                placeholder="请填写密码"
+                :rules="[{ required: true, message: '请填写密码' }]"
+            />
+            <van-field
+                v-model="checkPassword"
+                type="password"
+                name="checkPassword"
+                label="确认密码"
+                placeholder="请确认密码"
+                :rules="[{ required: true, message: '请确认密码' }]"
+            />
+          </van-cell-group>
+
+          <div style="margin: 16px;">
+            <van-button round block type="primary" native-type="submit">
+              注册
+            </van-button>
+          </div>
+        </van-form>
+      </div>
+    </van-tab>
+  </van-tabs>
 
 </template>
 
@@ -35,12 +78,13 @@ import {useRoute, useRouter} from "vue-router";
 const router = useRouter();
 const route = useRoute();
 
-const userAccount = ref('');
-const userPassword = ref('');
-const onSubmit = async () => {
+// =============== 登录部分 ===============
+const loginUserAccount = ref('');
+const loginUserPassword = ref('');
+const onLoginSubmit = async () => {
   const res = await myAxios.post('/user/login', {
-    userAccount: userAccount.value,
-    userPassword: userPassword.value
+    userAccount: loginUserAccount.value,
+    userPassword: loginUserPassword.value
   });
   console.log('用户登录\n', res);
   if (res.data.code === 0 && res.data.data) {
@@ -51,6 +95,24 @@ const onSubmit = async () => {
     Toast.fail('登录失败');
   }
 };
+
+// =============== 注册部分 ===============
+const registerUserPassword = ref('');
+const checkPassword = ref('');
+const onRegisterSubmit = async () => {
+  const res = await myAxios.post('/user/register', {
+    userPassword: registerUserPassword.value,
+    checkPassword: checkPassword.value
+  });
+  console.log('用户注册\n', res);
+  alert(JSON.stringify(res));
+  if (res.data.code === 0 && res.data.data) {
+    // 跳转到之前的页面
+    window.location.href = '/setting';
+  } else {
+    Toast.fail('注册失败');
+  }
+}
 </script>
 
 <style scoped>
