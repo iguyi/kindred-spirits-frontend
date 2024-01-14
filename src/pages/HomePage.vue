655aa462642bfd6839b3cc4a263110f1f186d639
guyi <1802366955@qq.com>
2024-01-14 23:07:06 +0800

    展示历史会话

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,19 @@
 <template>
-聊天页
+  <HistoryRoomCardList :chat-room-list="historyChatRoomList"/>
 </template>
 
 <script setup>
+import HistoryRoomCardList from "../components/HistoryRoomCardList.vue";
+import {onMounted, ref} from "vue";
+import myAxios from "../plugins/myAxios";
+
+const historyChatRoomList = ref([]);
+
+onMounted(async () => {
+  // 获取历史会话
+  const historyChatRoomListData = await myAxios.get("/chat/room/list", {});
+  historyChatRoomList.value = historyChatRoomListData.data.data;
+});
 
 </script>
 
