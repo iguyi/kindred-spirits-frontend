8c2ae307865e7acaeb6b8f2890532b8d784eb1d8
guyi <1802366955@qq.com>
2024-01-17 23:04:20 +0800

    自由搜索-用户

--- src/pages/search/SearchResultPage.vue
+++ src/pages/search/SearchResultPage.vue
@@ -11,31 +11,54 @@ import {Toast} from "vant";
 import qs from "qs";
 import UserCardList from "../../components/UserCardList.vue";
 
-const userList = ref([]);
 const route = useRoute();
-const {tags} = route.query;
-onMounted(async () => {
-  const userListData = await myAxios.get('/user/search/tags', {
-    params: {
-      tagNameList: tags,
-    },
-    paramsSerializer: params => {
-      // opts: arg[0]=1&arg[1]=2
-      return qs.stringify(params, {indices: false});
-    }
-  }).then(function (response) {
-    console.log('/user/search/tags succeed' + response);
-    return response.data?.data;
-  }).catch(function (error) {
-    console.log('/user/search/tags error' + error);
-    Toast.fail('搜索失败');
-  });
 
-  if (userListData) {
-    userListData.forEach(user => {
-      user.tags = JSON.parse(user.tags);
+/**
+ * 查询结果 - 用户列表
+ */
+const userList = ref([]);
+
+onMounted(async () => {
+  const {model, tags, searchCondition} = route.query;
+  if (model === '1') {
+    const userListData = await myAxios.get('/user/search/tags', {
+      params: {
+        tagNameList: tags,
+      },
+      paramsSerializer: params => {
+        // opts: arg[0]=1&arg[1]=2
+        return qs.stringify(params, {indices: false});
+      }
+    }).then(function (response) {
+      console.log('/user/search/tags succeed' + response);
+      return response.data?.data;
+    }).catch(function (error) {
+      console.log('/user/search/tags error' + error);
+      Toast.fail('搜索失败');
     });
-    userList.value = userListData;
+
+    if (userListData) {
+      userListData.forEach(user => {
+        user.tags = JSON.parse(user.tags);
+      });
+      userList.value = userListData;
+    }
+    return;
+  }
+
+  if (model === '2') {
+    const userListData = await myAxios.get('/user/search', {
+      params: {
+        searchCondition: searchCondition,
+      }
+    });
+    if (userListData) {
+      userListData.data.data.forEach(user => {
+        user.tags = JSON.parse(user.tags);
+      });
+      userList.value = userListData.data.data;
+    }
+    return;
   }
 });
 </script>
