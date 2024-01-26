5500e6a75dd119def9611c83e6457596a8b3c0ff
guyi <1802366955@qq.com>
2024-01-26 23:20:49 +0800

    进入消息通知页

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,4 +1,24 @@
 <template>
+  <van-cell
+      is-link
+      to="/room/message"
+      icon="envelop-o"
+      center
+  >
+    <template #icon>
+      <van-icon name="envelop-o" size="23px" style="margin-left: 5px"/>
+    </template>
+    <template #right-icon v-if="undressedNum !== 0">
+      <van-icon :badge="undressedNum" size="23px" style="margin-right: 25px"/>
+    </template>
+    <template #title>
+      <div style="font-weight: bold; font-size: 16px; margin-left: 5px">
+        消息通知
+      </div>
+    </template>
+
+  </van-cell>
+
   <HistoryRoomCardList :chat-room-list="historyChatRoomList"/>
 </template>
 
@@ -7,12 +27,20 @@ import HistoryRoomCardList from "../components/HistoryRoomCardList.vue";
 import {onMounted, ref} from "vue";
 import myAxios from "../plugins/myAxios";
 
+// 历史会话列表
 const historyChatRoomList = ref([]);
+
+// 未处理的消息数
+const undressedNum = ref(0);
 
 onMounted(async () => {
   // 获取历史会话
   const historyChatRoomListData = await myAxios.get("/chat/room/list", {});
   historyChatRoomList.value = historyChatRoomListData.data.data;
+
+  // 获取未读消息数
+  const messageListData = await myAxios.get("/message/undressed", {});
+  undressedNum.value = messageListData.data.data;
 });
 
 </script>
