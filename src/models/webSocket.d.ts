21a3a86977d7a3954f58581cddcfcfd813d28213
guyi <1802366955@qq.com>
2024-02-07 16:20:11 +0800

    WebSocket 签名

---
+++ src/models/webSocket.d.ts
@@ -0,0 +1,26 @@
+/**
+ * WebSocket 签名
+ */
+export type WebSocketType = {
+
+    /**
+     * 用户 id
+     */
+    id: number;
+
+    /**
+     * 用户账号
+     */
+    userAccount: string;
+
+    /**
+     * 用户昵称
+     */
+    username: string;
+
+    /**
+     * 用户头像
+     */
+    avatarUrl: string;
+
+}
