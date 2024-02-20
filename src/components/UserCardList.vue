c136a000989480b470929abc99cfaffda3757344
guyi <1802366955@qq.com>
2024-02-20 16:27:05 +0800

    普通匹配用户分页展示

--- src/components/UserCardList.vue
+++ src/components/UserCardList.vue
@@ -1,58 +1,67 @@
 <template>
-  <van-cell
-      v-for="user in userList"
-      center
+  <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
   >
-    <template #right-icon>
-    </template>
-    <!--  添加好友  -->
-    <template #extra>
-      <van-button
-          type="primary"
-          size="mini"
-          @click="showUser(user.id)"
-      >
-        查看
-      </van-button>
-      <van-button
-          type="primary"
-          size="mini"
-          @click="addFriend(user.id)"
-      >
-        添加
-      </van-button>
+    <van-cell
+        v-for="user in userList"
+        center
+    >
+      <template #right-icon>
+      </template>
+      <!--  添加好友  -->
+      <template #extra>
+        <van-button
+            type="primary"
+            size="mini"
+            @click="showUser(user.id)"
+        >
+          查看
+        </van-button>
+        <van-button
+            type="primary"
+            size="mini"
+            @click="addFriend(user.id)"
+        >
+          添加
+        </van-button>
 
-    </template>
-    <template #default>
-      <div class="custom-cell">
-        <!-- 头像 -->
-        <div>
-          <van-image
-              round
-              width="80px"
-              height="80px"
-              :src="user.avatarUrl"
-          />
-        </div>
-        <!-- 用户信息 -->
-        <div style="margin-left: 5px">
+      </template>
+      <template #default>
+        <div class="custom-cell">
+          <!-- 头像 -->
+          <div>
+            <van-image
+                round
+                width="80px"
+                height="80px"
+                :src="user.avatarUrl"
+            />
+          </div>
+          <!-- 用户信息 -->
+          <div style="margin-left: 5px">
           <span class="custom-title">
             {{ user.username.length <= 11 ? user.username : user.username.substring(0, 8) + '...' }}
           </span><br>
-          <van-tag class="custom-tag"
-                   v-for="(tag, index) in displayedTags(user.tags)" :key="index"
-                   type="danger"
-          >
-            {{ tag }}
-          </van-tag>
-          <br>
-          <span
-              v-if="user.profile">{{ user.profile.length < 10 ? user.profile : user.profile.substring(0, 10) + '...' }}
+            <van-tag class="custom-tag"
+                     v-for="(tag, index) in displayedTags(user.tags)" :key="index"
+                     type="danger"
+            >
+              {{ tag }}
+            </van-tag>
+            <br>
+            <span
+                v-if="user.profile">{{
+                user.profile.length < 10 ? user.profile : user.profile.substring(0, 10) + '...'
+              }}
           </span>
+          </div>
         </div>
-      </div>
-    </template>
-  </van-cell>
+      </template>
+    </van-cell>
+  </van-list>
 
   <van-dialog
       v-model:show="show"
@@ -76,6 +85,13 @@ import myAxios from "../plugins/myAxios";
 import {Toast} from "vant";
 import {ref} from "vue";
 import ShowUser from "./ShowUser.vue";
+import {userCarType} from "../states/userCar";
+
+// 列表相关参数
+const loading = ref(false);
+const finished = ref(false);
+const pageSize = ref(10);  // 每页 10 条数据
+const pageNum = ref(2);  // 初始页数为 2, 因为首次展示的数据是第一页的数据
 
 /**
  * 添加好友
@@ -139,13 +155,58 @@ const displayedTags = (tags: string[]) => {
   return tags.slice(0, 4);
 };
 
+/**
+ * 异步加载数据
+ */
+const onLoad = async () => {
+  if (props.flushPath === userCarType.match || props.flushPath===userCarType.search) {
+    // 无数据可加载
+    loading.value = false;
+    finished.value = true;
+    return;
+  }
+
+  let userListData = await myAxios.get(props.flushPath, {
+    params: {
+      pageSize: pageSize.value,
+      pageNum: pageNum.value
+    },
+  }).then(function (response) {
+    loading.value = false;
+    console.log(`${props.flushPath} succeed. ${response}`);
+    return response.data?.data;
+  }).catch(function (error) {
+    loading.value = false;
+    console.log(`{props.flushPath} error. ${error}`);
+    Toast.fail('系统繁忙');
+    return;
+  });
+
+  if (userListData === null || userListData.length === 0) {
+    // 无数据可加载
+    loading.value = false;
+    finished.value = true;
+    return;
+  }
+  userListData.forEach(user => {
+    if (user.tags) {
+      user.tags = JSON.parse(user.tags);
+    }
+    props.userList.push(user);
+  });
+  pageNum.value = pageNum.value + 1;
+  loading.value = false;
+};
+
 interface UserCardListProps {
   userList: UserType[];
+  flushPath: string;
 }
 
 const props = withDefaults(defineProps<UserCardListProps>(), {
   // @ts-ignore
   userList: [] as UserType[],
+  flushPath: '' as string
 });
 </script>
 
