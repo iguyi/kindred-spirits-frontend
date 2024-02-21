f23b4b1db4b80ddda863950b8566a6669933dcc4
guyi <1802366955@qq.com>
2024-02-21 14:07:25 +0800

    分页参数

--- src/config/page.ts
+++ src/config/page.ts
@@ -1,11 +1,11 @@
 // 分页配置
 
 // 基本分页参数
-const basePageSize = 10;
-const basePageNumInit = 1;
+export const basePageSize = 10;
+export const basePageNumInit = 1;
 
 // 心动模式分页参数
-const likePageSize = 20;
+export const likePageSize = 20;
 
 export default {
     basePageSize,
